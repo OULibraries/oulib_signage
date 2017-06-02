15ab4a85716bd56f36655b90f546fae6eebccca6
c-corbett <ccorbett@ou.edu>
2017-06-02 16:10:58 -0500

    Tweaking page template, adjusting CSS.

--- templates/page.tpl.php
+++ templates/page.tpl.php
@@ -95,25 +95,25 @@
 <div class="container-fluid">
 <div class="row">
 	<?php if (!empty($page['logo'])): ?>
-		<div class="col-md-7 col-sm-12 col-xs-12">
+		<div class="col-md-8 col-sm-9 col-xs-8">
 			<?php print render($page['logo']); ?>
 		</div>
 	<?php endif; ?>
 	
 
-	 <div class="col-md-5 col-sm-12 col-xs-12">
+	 <div class="col-md-4 col-sm-3 col-xs-4">
                 <div class="login-area">
-                    <div class="col-md-7 col-sm-12 col-xs-12">
+                    <div class="col-md-5 col-sm-12 col-xs-12">
                       <?php if (!empty($page['login-left'])): ?>
                         <?php print render($page['login-left']); ?>
                       <?php endif; ?>
                     </div>
-                    <div class="col-md-2 hidden-sm hidden-xs">
+                    <div class="col-md-3 hidden-sm hidden-xs">
                       <?php if (!empty($page['login-dots'])): ?>
                         <?php print render($page['login-dots']); ?>
                       <?php endif; ?>
                     </div>
-                    <div class="col-md-3 col-sm-12 col-xs-12">
+                    <div class="col-md-4 col-sm-12 col-xs-12">
                       <?php if (!empty($page['login-right'])): ?>
                         <?php print render($page['login-right']); ?>
                       <?php endif; ?>
